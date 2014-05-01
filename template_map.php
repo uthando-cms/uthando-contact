79f90db4e888315b5faaae75a6c0d780d14e67ea
shaunfreeman <shaun@shaunfreeman.co.uk>
2014-05-01 11:03:28 +0100

    some styling changes

--- template_map.php
+++ template_map.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'uthando-contact/contact/index'     => __DIR__ . '/view/uthando-contact/contact/index.phtml',
-    'uthand-contact/contact/thank-you'  => __DIR__ . '/view/uthando-contact/contact/thank-you.phtml',
+    'uthando-contact/contact/index'             => __DIR__ . '/view/uthando-contact/contact/index.phtml',
+    'uthand-contact/contact/thank-you'          => __DIR__ . '/view/uthando-contact/contact/thank-you.phtml',
 ];
