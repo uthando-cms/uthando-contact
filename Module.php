24fff78fbf9e53ffb6e9977b67b4882fe1d44413
shaunfreeman <shaun@shaunfreeman.co.uk>
2014-09-29 21:19:42 +0100

    module updates

--- Module.php
+++ Module.php
@@ -5,30 +5,7 @@ class Module
 {
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
-    }
-    
-    public function getServiceConfig()
-    {
-        return [
-            'invokables'    => [
-                'UthandoContact\InputFilter\Contact'    => 'UthandoContact\InputFilter\Contact',
-                'UthandoContact\Service\Contact'        => 'UthandoContact\Service\Contact',
-            ],
-            'factories'     => [
-                'UthandoContact\Service\MailMessage'    => 'UthandoContact\Service\MailMessageFactory',
-                'UthandoContact\Service\MailTransport'  => 'UthandoContact\Service\MailTransportFactory',
-            ],
-        ];
-    }
-    
-    public function getViewHelperConfig()
-    {
-        return [
-            'invokables' => [
-                'Contact' => 'UthandoContact\View\Contact',
-            ],
-        ];
+        return include __DIR__ . '/config/config.php';
     }
     
     public function getControllerConfig()
@@ -48,6 +25,29 @@ class Module
             	'ContactCaptcha'  => 'UthandoContact\Form\Element\Captcha',
         	],
     	];
+    }
+
+    public function getServiceConfig()
+    {
+        return [
+            'invokables'    => [
+                'UthandoContact\InputFilter\Contact'    => 'UthandoContact\InputFilter\Contact',
+                'UthandoContact\Service\Contact'        => 'UthandoContact\Service\Contact',
+            ],
+            'factories'     => [
+                'UthandoContact\Service\MailMessage'    => 'UthandoContact\Service\MailMessageFactory',
+                'UthandoContact\Service\MailTransport'  => 'UthandoContact\Service\MailTransportFactory',
+            ],
+        ];
+    }
+
+    public function getViewHelperConfig()
+    {
+        return [
+            'invokables' => [
+                'Contact' => 'UthandoContact\View\Contact',
+            ],
+        ];
     }
     
     public function getAutoloaderConfig()
