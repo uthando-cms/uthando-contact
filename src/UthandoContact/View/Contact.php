6c1de0572431d00b52161fd83d9aa18282ad7c59
shaunfreeman <shaun@shaunfreeman.co.uk>
2014-12-12 09:52:33 +0000

    added simple retrival of config values

--- src/UthandoContact/View/Contact.php
+++ src/UthandoContact/View/Contact.php
@@ -2,13 +2,57 @@
 namespace UthandoContact\View;
 
 use UthandoCommon\View\AbstractViewHelper;
+use Zend\Config\Config;
 
 class Contact extends AbstractViewHelper
 {
-    public function __invoke()
+    /**
+     * @var Config
+     */
+    protected $contactConfig;
+
+    /**
+     * @param null $key
+     * @return $this|Config|string
+     */
+    public function __invoke($key=null)
     {
-        $config = $this->getConfig('contact');
-           
-        return $config;
+        if (!$this->contactConfig instanceof Config) {
+            $config = $this->getConfig('contact');
+
+            if (!$config instanceof Config) {
+                $config = new Config($config);
+            }
+
+            $this->contactConfig = $config;
+        }
+
+        if (is_string($key)) {
+            return $this->get($key);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param $key
+     * @return Config|string|null
+     */
+    public function get($key)
+    {
+        $keys = explode('.', $key);
+        $returnValue = null;
+        $config = $this->contactConfig;
+
+        foreach ($keys as $key) {
+
+            $returnValue = $config->get($key);
+
+            if ($returnValue instanceof Config) {
+                $config = $returnValue;
+            }
+        }
+
+        return $returnValue;
     }
 }
