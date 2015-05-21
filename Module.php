f947623b9e64dfe26b3cdb11f0f7b46364462493
shaunfreeman <shaun@shaunfreeman.co.uk>
2015-05-21 14:14:09 +0100

    tidyup

--- Module.php
+++ Module.php
@@ -5,35 +5,7 @@ class Module
 {
     public function getConfig()
     {
-        return include __DIR__ . '/config/config.php';
-    }
-    
-    public function getControllerConfig()
-    {
-        return [
-            'invokables' => [
-        	   'UthandoContact\Controller\Contact'    => 'UthandoContact\Controller\ContactController',
-           ],
-        ];
-    }
-
-    public function getServiceConfig()
-    {
-        return [
-            'invokables'    => [
-                'UthandoContact\InputFilter\Contact'    => 'UthandoContact\InputFilter\Contact',
-                'UthandoContact\Service\Contact'        => 'UthandoContact\Service\Contact',
-            ],
-        ];
-    }
-
-    public function getViewHelperConfig()
-    {
-        return [
-            'invokables' => [
-                'Contact' => 'UthandoContact\View\Contact',
-            ],
-        ];
+        return include __DIR__ . '/config/module.config.php';
     }
     
     public function getAutoloaderConfig()
--- config/module.config.php
+++ config/module.config.php
@@ -52,6 +52,25 @@ return [
             ],
         ],
     ],
+    'controllers' => [
+        'invokables' => [
+            'UthandoContact\Controller\Contact' => 'UthandoContact\Controller\ContactController',
+        ],
+    ],
+    'service_manager' => [
+        'invokables'    => [
+            'UthandoContact\InputFilter\Contact'    => 'UthandoContact\InputFilter\Contact',
+            'UthandoContact\Service\Contact'        => 'UthandoContact\Service\Contact',
+        ],
+    ],
+    'view_helpers' => [
+        'invokables' => [
+            'Contact' => 'UthandoContact\View\Contact',
+        ],
+    ],
+    'view_manager'  => [
+        'template_map' => include __DIR__ . '/../template_map.php'
+    ],
     'router' => [
         'routes' => [
             'contact' => [
@@ -96,8 +115,5 @@ return [
                 'route' => 'contact',
             ],
         ],
-    ],
-    'view_manager'  => [
-        'template_map' => include __DIR__ . '/../template_map.php'
     ],
 ];
