7d7d19f3a5483f239b2cd90b08968f30109c2c67
shaunfreeman <shaun@shaunfreeman.co.uk>
2016-03-16 10:02:22 +0000

    added phpsettings to build

--- test/autoload/uthando.global.php
+++ test/autoload/uthando.global.php
@@ -2,6 +2,15 @@
 
 return [
     'load_uthando_configs' => false,
+    'php_settings' => [
+        'display_startup_errors'        => true,
+        'display_errors'                => true,
+        'error_reporting'               => E_ALL ^ E_USER_DEPRECATED,
+        'max_execution_time'            => 60,
+        'date.timezone'                 => 'Europe/London',
+        'zlib.output_compression'       => true,
+        'zlib.output_compression_level' => -1,
+    ],
     'uthando_contact' => [
         'form' => [
             'name' => 'ContactForm',
